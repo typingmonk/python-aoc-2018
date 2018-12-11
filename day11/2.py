a91c872b2167f08de452fff86d471aff513dceec
tmonk <felixtypingmonkey@gmail.com>
2018-12-12 06:13:56 +0800

    refine 2.py

--- day11/2.py
+++ day11/2.py
@@ -1,39 +1,62 @@
-def getDigit(number):
-    string = str(number)
-    if len(string) >= 3:
-        return int(string[len(string)-3])
-    else:
-        return 0
-
-def getPL(x,y,grid,s):
-    total = 0
-    for i in range(s):
-        for j in range(s):
-                total += grid[y+i][x+j]
-    return total
-
 input = 9798
-grid  = []
+grid     = []
+sa_table = []
+
+def pLevel(x,y,input):
+    rID   = x + 10
+    power = (y * rID + input) * rID
+    power = ((power // 100) % 10 ) - 5
+    return power
+
+def sum_area(x,y,grid,sa_table):
+    ## s(x,y) = s(x-1,y) + s(x,y-1) - s(x-1,y-1) + p(x,y)
+    ## s(x,y) =    A     +    B     -     C      + D
+    A = 0 if x-1 < 0 else sa_table[y][x-1]
+    B = 0 if y-1 < 0 else sa_table[y-1][x]
+    C = 0 if x-1 < 0 or y-1 < 0 else sa_table[y-1][x-1]
+    D = grid[y][x]
+    return A + B - C + D
+
+def sum_area_by_size(x,y,s,sa_table):
+    ## s(x,y) = s(x+s-1,y+s-1) - ( s(x+s-1,y-1) + s(x-1,y+s-1) - s(x-1,y-1))
+    ## s(x,y) = s(x+s-1,y+s-1) + s(x-1,y-1) - s(x+s-1,y-1) - s(x-1,y+s-1)
+    ## s(x,y) =       A        +      B     -      C       -       D
+    A = sa_table[y+s-1][x+s-1]
+    B = 0 if x-1 < 0 or y-1 < 0 else sa_table[y-1][x-1]
+    C = 0 if y-1 < 0 else sa_table[y-1][x+s-1]
+    D = 0 if x-1 < 0 else sa_table[y+s-1][x-1]
+    return A + B - C - D
+
 for y in range(300):
     line = []
     for x in range(300):
-        rID = x + 10
-        line.append(getDigit(rID * (rID * y + input)) - 5)
+        line.append(pLevel(x,y,input))
     grid.append(line)
 
-pLevel_per_size = []
-locations       = []
-for size in range(2,14,1):
-        print("size",size)
-        p_levels = []
-        for y in range(300 - size):
-                for x in range(300 - size):
-                        p_levels.append(getPL(x,y,grid,size))
-        idx = p_levels.index(max(p_levels))
-        X = idx %  (300 - size)
-        Y = idx // (300 - size)
-        pLevel_per_size.append(max(p_levels))
-        locations.append((X,Y))
-idx = pLevel_per_size.index(max(pLevel_per_size))
-print(idx+2)
-print(locations[idx][0],locations[idx][1],idx+2)
+for y in range(300):
+    line = []
+    for x in range(300):
+        line.append(0)
+    sa_table.append(line)
+
+for y in range(300):
+    for x in range(300):
+        sa_table[y][x] = sum_area(x,y,grid,sa_table)
+
+locations = []
+pLevels   = []
+for size in range(2,301):
+    candidates = []
+    for y in range(301-size):
+        for x in range(301-size):
+            candidates.append(sum_area_by_size(x,y,size,sa_table))
+    idx = candidates.index(max(candidates))
+    X   = idx  %  (301-size)
+    Y   = idx  // (301-size)
+    pLevels.append(candidates[idx])
+    locations.append((X,Y))
+
+idx      = pLevels.index(max(pLevels))
+size     = idx + 2
+location = locations[idx]
+print(location[0],location[1],size)
--- day11/2alter.py
+++ day11/2alter.py
@@ -1,23 +1,17 @@
-from collections import defaultdict
+import numpy
 serial = 9798
-grid_sums, partial_sums = {}, defaultdict(int)
 
-power_level = lambda x, y: ((((x + 10) * y + serial) * (x + 10)) // 10 ** 2 % 10) - 5
-calculate_ps = lambda x, y: (power_level(x + 1, y + 1)
-                             + partial_sums[x, y-1] + partial_sums[x-1, y] - partial_sums[x-1, y-1])
+def power(x, y):
+    rack = (x + 1) + 10
+    power = rack * (y + 1)
+    power += serial
+    power *= rack
+    return (power // 100 % 10) - 5
 
-for j in range(300):
-    for i in range(300):
-        partial_sums[(i, j)] = calculate_ps(i, j)
+grid = numpy.fromfunction(power, (300, 300))
 
-for size in range(2, 300):
-    for j in range(size-1, 300):
-        for i in range(size-1, 300):
-            gp = partial_sums[(i, j)] + partial_sums[(i-size, j-size)] \
-                 - partial_sums[(i-size, j)] - partial_sums[(i, j-size)]
-            grid_sums[gp] = (i-size+2, j-size+2, size)
-    if size == 3:
-        x3, y3, s3 = map(str, grid_sums[max(grid_sums)])
-        print("Day 11 part 1: " + x3 + "," + y3)
-
-print("Day 11 part 2: %d,%d,%d" % grid_sums[max(grid_sums)])
+for width in range(3, 300):
+    windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
+    maximum = int(windows.max())
+    location = numpy.where(windows == maximum)
+    print(width, maximum, location[0][0] + 1, location[1][0] + 1)
